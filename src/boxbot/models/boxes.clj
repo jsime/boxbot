3276e901081798d123f9f0694bb711a2e4a28162
Jon Sime <jonsime@gmail.com>
2016-12-30 05:53:13 +0000

    update modules/boxes entity fields

--- src/boxbot/models/boxes.clj
+++ src/boxbot/models/boxes.clj
@@ -4,7 +4,7 @@
 (k/defentity boxes
   (k/pk :box_id)
   (k/table :boxes)
-  (k/entity-fields :name :description))
+  (k/entity-fields :box_id :user_id :location_id :name :description :created_at))
 
 (defn all
   []
