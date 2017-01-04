3cf6f9b4bbca23d50395512760061aaedef60091
Jon Sime <jonsime@gmail.com>
2017-01-04 01:31:46 +0000

    recurse the chain of parents and return them with their child locations

--- src/boxbot/models/locations.clj
+++ src/boxbot/models/locations.clj
@@ -5,11 +5,17 @@
 
 (defn create
   [user data]
-  ;; TODO Ensure that user actually owns the location specified in :parent_id
-  (let [loc (select-keys data [:parent_id :name :description])]
+  (let [loc (select-keys data [:name :description])
+        parent-id (try (Integer. (:parent_id data)) (catch Exception e))
+        parent (if parent-id (first (k/select e/locations
+                               (k/where {:user_id (:user_id user)
+                                         :location_id parent-id}))))]
+    (if (and parent-id (nil? parent))
+      (throw (Exception. "invalid parent location ID")))
     (k/insert e/locations
       (k/values
-        (assoc loc :user_id (:user_id user))))))
+        (assoc loc :user_id (:user_id user)
+                   :parent_id parent-id)))))
 
 (defn user-locations
   [user-id]
@@ -18,6 +24,21 @@
 
 (defn user-location
   [user-id loc-id]
-  (first
-    (k/select e/locations
-      (k/where {:user_id user-id :location_id loc-id}))))
+  (let [loc (first
+              (k/select e/locations
+                (k/where {:user_id user-id :location_id loc-id})))]
+    (if loc
+      (assoc loc :parents (k/exec-raw [(str
+        "WITH RECURSIVE r AS ("
+        "       SELECT  l.*, array[l.location_id] as path, 0 as level"
+        "         FROM  locations l"
+        "        WHERE  l.parent_id is null"
+        "        UNION ALL"
+        "       SELECT  l2.*, r.path || l2.location_id as path, r.level + 1 as level"
+        "         FROM  locations l2"
+        "         JOIN r on l2.parent_id = r.location_id"
+        ") SELECT  r2.location_id, r2.name, r2.parent_id, r2.description"
+        "    FROM  (SELECT unnest(r.path) FROM r WHERE r.user_id = ? and r.location_id = ?) x(id)"
+        "    JOIN  r r2 on (r2.location_id = x.id)"
+        "   WHERE  r2.location_id != ?"
+        "ORDER BY  r2.level asc") [user-id loc-id loc-id]] :results)))))
